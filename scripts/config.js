2c13d3830ca2db4a3436e3903c02e4f470c815ac
Isaac Pierce <contact@isaacdpierce.com>
2019-03-10 22:55:14 -0700

    Get

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,4 @@
-const config = {
-  apiKey: '',
-  searchURL: '',
+const configUnsplash = {
+  apiKey: '5f040341adf347c74dc5b43e7b14be6c85b0423acb0c044b3897b26f534d5ead',
+  searchURL: 'https://api.unsplash.com/search/photos/',
 };
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// https://api.unsplash.com/search/photos/?page=1&per_page=10&query=${query}&client_id=${cred.APP_ID}
+
 function formatQueryParams(params) {
   const queryItems = Object.keys(params).map(
     key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
@@ -7,34 +9,44 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function displayResults(responseJson) {
-  let parks = responseJson.data;
+function displayResultsUnsplash(responseJson) {
+  let images = responseJson.results;
+  console.log(images);
 
   $('#results-list').empty();
 
-  for (let park of parks) {
-    let { fullName, description, url, directionsUrl } = park;
+  for (let image of images) {
+    let imageUrl = image.urls.regular;
+    let artist = image.user.name;
+    let portfolio = image.user.links.html;
+
+    console.log(imageUrl);
+    console.log(artist);
+    console.log(portfolio);
 
     $('#results-list').append(
-      `<li><h3>${fullName}</h3>
-      <p>${description}</p>
-      <a href="${url}">Website</a>
-      <a href="${directionsUrl} target="_blank">Directions</a>
+      `<li>        
+       <a class="unsplash-image" href="${imageUrl}" target="_blank"><img src=${imageUrl} alt="No result"></a>
+       <a class="unsplash-image__details" href=${portfolio} target="_blank"><span>${artist}</span></a>
       </li>`
     );
   }
 
   $('#results').removeClass('hidden');
+  $('#particles-js').addClass('hidden');
 }
 
-function getParks(stateCode, limit = 10) {
+function getUnsplashImages(query, limit = 10) {
   const params = {
-    stateCode,
-    limit,
-    apiKey: config.apiKey,
+    per_page: limit,
+    query,
+    client_id: configUnsplash.apiKey,
   };
   const queryString = formatQueryParams(params);
-  const url = `${config.searchURL}?${queryString}`;
+  const url = `${configUnsplash.searchURL}?${queryString}`;
+  // TODO remove log
+  console.log(queryString);
+  console.log(url);
 
   fetch(url)
     .then(response => {
@@ -43,20 +55,43 @@ function getParks(stateCode, limit = 10) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson))
+
+    .then(responseJson => displayResultsUnsplash(responseJson))
+
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
 
+// function getIcons(stateCode, limit = 10) {
+//   const params = {
+//     stateCode,
+//     limit,
+//     apiKey: config.apiKey,
+//   };
+//   const queryString = formatQueryParams(params);
+//   const url = `${config.searchURL}?${queryString}`;
+
+//   fetch(url)
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       throw new Error(response.statusText);
+//     })
+//     .then(responseJson => displayResults(responseJson))
+//     .catch(err => {
+//       $('#js-error-message').text(`Something went wrong: ${err.message}`);
+//     });
+// }
+
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    // Remove spaces from search for proper url building
-    const stateCode = searchTerm.replace(/\s+/g, '');
+    const query = $('#js-search-term').val();
     const limit = $('#js-max-results').val();
-    getParks(stateCode, limit);
+    getUnsplashImages(query, limit);
+    $('html').scrollTop(0);
   });
 }
 
